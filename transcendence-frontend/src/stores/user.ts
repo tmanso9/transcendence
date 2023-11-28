ab90ad7457f6973ebe6f85fd3d6e03c22f6a9bf6
tmanso9 <tmanso92@gmail.com>
2023-11-28 16:08:08 +0000

    added user store

---
+++ transcendence-frontend/src/stores/user.ts
@@ -0,0 +1,19 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', () => {
+  const name = ref('')
+  const isAdmin = ref(false)
+
+  const login = () => {
+	  name.value = 'touteiro'
+	  console.log('Login successful')
+  }
+
+  const logout = () => {
+	console.log('Logging out')
+	name.value = ''
+  }
+
+  return { name, isAdmin, login, logout}
+})
