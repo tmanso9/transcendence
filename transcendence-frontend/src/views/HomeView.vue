4e8e29bbc3602e6c15a82135a14eeeb48b2e9a7e
tmanso9 <tmanso92@gmail.com>
2023-11-28 14:10:33 +0000

    Login mock

--- transcendence-frontend/src/views/HomeView.vue
+++ transcendence-frontend/src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
     <p>This is the home page</p>
+    <p>
+      You can try logging in <a href="/auth/login" @click.prevent="login">here</a> (check the
+      console for now)
+    </p>
   </main>
 </template>
+
+<script lang="ts" setup>
+const login = () => {
+  console.log('Login attempt')
+}
+</script>
