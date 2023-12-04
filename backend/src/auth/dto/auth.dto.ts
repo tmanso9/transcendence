8bf6ad982bb561fae9363900db3460efb7d1b427
Gabriel Tocha <84543830+portugueseTorch@users.noreply.github.com>
2023-12-04 15:52:11 +0000

    Backend (#4)

---
+++ backend/src/auth/dto/auth.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator"
+
+export class AuthDTO {
+	@IsEmail()
+	@IsNotEmpty()
+	email: string
+
+	@IsString()
+	@IsNotEmpty()
+	password: string
+
+	@IsString()
+	@IsOptional()
+	username: string
+}
