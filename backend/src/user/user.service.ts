7ce82baab86a28307d660330723e9f7da122d098
tmanso9 <77019593+tmanso9@users.noreply.github.com>
2023-12-04 16:36:48 +0000

    Added get_users endpoint (#6)
    Co-authored-by: Gabriel Tocha <gabriel.tocha@gmail.com>

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+	constructor(private prisma: PrismaService) {}
+
+	async getUsers() {
+		const all_users = await this.prisma.user.findMany({
+			select: {
+				username: true,
+				wins: true,
+				losses: true,
+				points: true
+			}
+		});
+
+		return all_users;
+	}
 	getMe(user: any) {
 		return user;
 	}
