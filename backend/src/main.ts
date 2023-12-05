a473a51ff9b2a3f886998bdeb937a74124c29a22
tmanso9 <tmanso92@gmail.com>
2023-12-05 13:17:27 +0000

    trying login, cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,17 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3001', // Replace with your frontend's origin
+    methods: ['GET', 'POST'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(3000);
 }
 bootstrap();
