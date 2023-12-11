ba6e97c3329a20635290f34bebc5ef5a8555c1e9
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-12-11 13:05:47 +0000

    group's setting on going

--- frontend/src/components/chat/ChannelSettings.vue
+++ frontend/src/components/chat/ChannelSettings.vue
@@ -1,39 +1,59 @@
 <script setup lang="ts">
 import { chatAppStore } from "@/store/chat";
+import { ref } from "vue";
 
 const store = chatAppStore();
+const channelSettings = ref(false);
+const channel = ref(store.getChannelInfo(store.selectedChannel));
 </script>
 <template>
-  <v-col cols="auto">
-    <v-dialog transition="dialog-top-transition" width="auto">
-      <template v-slot:activator="{ props }">
-        <v-icon icon="mdi-cog" size="x-small" v-bind="props"></v-icon>
-      </template>
-      <template v-slot:default="{ isActive }">
-        <v-card class="channelSettingsPopUp">
-          <v-toolbar
-            color="primary"
-            :title="store.selectedChannel + ' - Settings'"
-          ></v-toolbar>
-          <v-card-text>
-            <v-text-field
-              label="Change Password"
-              placeholder="New"
-              variant="outlined"
-            ></v-text-field>
-            <v-autocomplete
-              :items="store.getChannelInfo(store.selectedChannel)?.members"
-              label="Blocked"
-              color="primary"
-              multiple
-            ></v-autocomplete>
-          </v-card-text>
-          <v-card-actions class="justify-end">
-            <v-btn variant="text" @click="isActive.value = false">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </template>
-    </v-dialog>
-  </v-col>
+  <v-icon
+    icon="mdi-cog"
+    size="x-small"
+    @click="
+      () => {
+        channelSettings = true;
+      }
+    "
+  ></v-icon>
+  <div v-if="channelSettings" class="channelSettings">
+    <div class="channelSettings-header">
+      <span class="text-h6"
+        ><v-icon icon="mdi-cog" size="x-small"></v-icon
+        >{{ " " + store.selectedChannel }}
+      </span>
+      <v-icon
+        icon="mdi-close-circle"
+        color="primary"
+        @click="
+          () => {
+            channelSettings = false;
+          }
+        "
+      ></v-icon>
+    </div>
+    <div class="channelSettings-content">
+      <div class="channelSettings-content-profile">
+        <v-icon :icon="channel?.avatar" size="x-large"></v-icon>
+      </div>
+      <div class="channelSettings-content-info">
+        {{ channel?.type + " - " }}
+        {{ channel?.members.length + " " }}
+        members
+      </div>
+      <v-virtual-scroll
+        :items="channel?.members"
+        height="250"
+        class="channelSettings-content-users"
+      >
+        <template v-slot:default="{ item }"
+          ><div class="channelSettings-content-users-user">
+            {{ item }}
+          </div></template
+        >
+      </v-virtual-scroll>
+      <v-btn color="primary">Leave Group</v-btn>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss"></style>
--- frontend/src/components/chat/MessagesScroll.vue
+++ frontend/src/components/chat/MessagesScroll.vue
@@ -7,7 +7,7 @@ const store = chatAppStore();
   <div class="messageScroll">
     <v-virtual-scroll
       :items="store.channelMessages(store.selectedChannel)"
-      height="410"
+      height="400"
       id="scrollMessages"
     >
       <template v-slot:default="{ item }">
