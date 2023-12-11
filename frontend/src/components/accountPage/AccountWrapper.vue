90e3168f494da2aeab92ceb81b8dbf65b9e848e1
tmanso9 <tmanso92@gmail.com>
2023-12-11 19:13:59 +0000

    added friends to mock data, started cards

--- frontend/src/components/accountPage/AccountWrapper.vue
+++ frontend/src/components/accountPage/AccountWrapper.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- <h3>User stats are here</h3> -->
   <avatar-wrapper :user="user"/>
+  <friends-wrapper class="text-center mt-5" :user="user"/>
 </template>
 
 <script lang="ts" setup>
 import AvatarWrapper from "./Avatar/AvatarWrapper.vue"
+import FriendsWrapper from "./Friends/FriendsWrapper.vue";
 
 const props = defineProps(["user"]);
 </script>
--- frontend/src/components/accountPage/Friends/FriendsWrapper.vue
+++ frontend/src/components/accountPage/Friends/FriendsWrapper.vue
@@ -1,4 +1,22 @@
 <template>
-
+  <h2>Friends</h2>
+  <div class="friends d-flex">
+    <v-card
+      v-for="(friend, i) in friends"
+      :key="i"
+      class="ma-2 w-25 d-flex align-center"
+      height="60px"
+    >
+      <v-avatar size="38px" class="mx-2">
+        <v-img :src="user.avatar" alt="friend avatar" />
+      </v-avatar>
+      {{ friend.username }}
+    </v-card>
+  </div>
 </template>
 
+<script lang="ts" setup>
+const props = defineProps(["user"]);
+
+const friends = props.user?.friends || [];
+</script>
