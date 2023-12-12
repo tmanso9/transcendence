efb52c4ca9eb7e81faf51bb551ad19b8fc552b5f
afonsomlcarvalho <afonsomlc28@gmail.com>
2023-12-12 11:19:46 +0000

    Blacklist validation and cleanup

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { LogoutMiddleware } from 'src/auth/middleware';
 
 @Module({
   controllers: [UserController],
   providers: [UserService]
 })
-export class UserModule {}
+export class UserModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogoutMiddleware)
+      .forRoutes('users/me');
+  }
+}
