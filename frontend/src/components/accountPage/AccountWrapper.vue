a2a43cb15fb6dc9eec405189f3d4d28ce11b7b75
tmanso9 <tmanso92@gmail.com>
2023-12-18 11:34:04 +0000

    implemented separate InteractButtons component

--- frontend/src/components/accountPage/AccountWrapper.vue
+++ frontend/src/components/accountPage/AccountWrapper.vue
@@ -2,58 +2,33 @@
   <!-- <h3>User stats are here</h3> -->
   <account-header
     class="d-flex ma-5 mx-auto w-75 justify-space-around align-center"
-    :user="user"
+    :account="account"
   />
-  <div
-    class="d-flex flex-column flex-sm-row justify-space-evenly mx-auto my-10 account__header__buttons"
-    :width="mdAndUp ? '80%' : '100%'"
-  >
-    <v-btn
-      v-for="button in headerButtons"
-      :key="button.text"
-      :width="buttonSize"
-      class="mx-auto my-2 my-sm-0 account__header__buttons__button py-3 d-flex align-center"
-      :color="button.color"
-    >
-      <v-icon>{{ button.icon }}</v-icon>
-      <span class="ml-2">{{ button.text }}</span>
-    </v-btn>
-  </div>
+  <interact-buttons :account="account" />
   <v-expansion-panels variant="popout" class="my-5">
-    <friends-wrapper class="" :user="user" />
-    <stats-wrapper class="" :stats="computedStats" :rank="user.rank" />
+    <friends-wrapper class="" :account="account" />
+    <stats-wrapper class="" :stats="computedStats" :rank="account.rank" />
     <match-history class="" />
   </v-expansion-panels>
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
-import { useDisplay } from "vuetify";
 import AccountHeader from "./Header/AccountHeader.vue";
 import FriendsWrapper from "./Friends/FriendsWrapper.vue";
 import StatsWrapper from "./Stats/StatsWrapper.vue";
 import MatchHistory from "./Stats/MatchHistory.vue";
+import InteractButtons from "./InteractButtons.vue";
 
-const props = defineProps(["user"]);
-const { sm, mdAndUp } = useDisplay();
+const props = defineProps(["account"]);
 
 const computedStats = computed(() => {
-  const { wins, losses, points } = props.user;
+  const { wins, losses, points } = props.account;
   return {
     wins: { name: "wins", value: wins },
     losses: { name: "losses", value: losses },
     points: { name: "points", value: points },
   };
-});
-
-const headerButtons = [
-  { text: "Chat", icon: "mdi-chat-outline" },
-  { text: "Add friend", icon: "mdi-account-plus-outline" },
-  { text: "Play pong", icon: "mdi-sword-cross", color: "deep-purple-darken-3" },
-];
-
-const buttonSize = computed(() => {
-  return sm.value || mdAndUp.value ? "145px" : "75%";
 });
 </script>
 
