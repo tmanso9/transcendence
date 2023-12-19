4fbf4aeda90bef0623e108c444be581f6bf64ac4
afonsomlcarvalho <afonsomlc28@gmail.com>
2023-12-19 10:58:09 +0000

    refresh token

---
+++ backend/prisma/migrations/20231216164126_removed_sub_from_blacklist/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sub` on the `Blacklist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Blacklist" DROP COLUMN "sub";
