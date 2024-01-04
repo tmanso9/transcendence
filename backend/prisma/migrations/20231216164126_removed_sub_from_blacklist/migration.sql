e5e7425afa52566364ab5b57a57c3759ae6cbde5
tmanso9 <tmanso92@gmail.com>
2024-01-04 14:52:05 +0000

    fix after pull request

--- backend/prisma/migrations/20231216164126_removed_sub_from_blacklist/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sub` on the `Blacklist` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Blacklist" DROP COLUMN "sub";
--- backend/prisma/migrations/20231218160929_added_rank/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Rank" AS ENUM ('GURU', 'PRO', 'NOOBIE');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "rank" "Rank" NOT NULL DEFAULT 'NOOBIE';
