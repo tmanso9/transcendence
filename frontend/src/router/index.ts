94a98adef5b5cea2fb6924b87440481a44ded719
polltter <miguelr.silva@outlook.pt>
2024-01-12 10:52:11 +0000

    fixed router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,30 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import PlayView from '../views/PlayView.vue';
 import RankingView from '@/views/RankingView.vue';
+import TwoFAView from '../views/TwoFAView.vue';
+import FirstLoginView from '../views/FirstLoginView.vue';
+import AccountView from '../views/AccountView.vue';
+import NotFound from '../views/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
     {
-      path: '/play',
-      component: () => import('../views/PlayView.vue')
-    },
-    {
-      path: '/game',
-      component: () => import('../views/GameView.vue')
+      path: "/play",
+      name: "play",
+      component: PlayView,
     },
     {
       path: "/ranking",
@@ -32,14 +32,30 @@ const router = createRouter({
       component: RankingView,
     },
     {
-      path: '/users/:username',
-      component: () => import('../views/AccountView.vue')
+      path: "/2fa",
+      name: "2fa",
+      component: TwoFAView,
     },
     {
-      path: '/:catchAll(.*)',
-      component: () => import('../views/NotFound.vue')
-    }
-  ]
-})
+      path: "/firstLogin",
+      name: "firstLogin",
+      component: FirstLoginView,
+    },
+    {
+      path: "/users/:username",
+      name: "account",
+      component: AccountView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "catchAll",
+      component: NotFound,
+    },
+    {
+      path: '/game',
+      component: () => import('../views/GameView.vue')
+    },
+  ],
+});
 
 export default router
