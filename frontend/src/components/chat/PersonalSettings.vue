022d2422c814cb0cbedd68a220cfdac4c754fbce
tmanso9 <tmanso92@gmail.com>
2024-01-12 17:12:27 +0000

    button disabled if user online

--- frontend/src/components/chat/PersonalSettings.vue
+++ frontend/src/components/chat/PersonalSettings.vue
@@ -43,13 +43,7 @@ const store = chatAppStore();
           >Profile</v-btn
         >
         <v-btn
-          v-if="store.selectedUserProfile?.status == 'ONLINE'"
-          class="channelSettings-content-buttons-btn"
-          append-icon="mdi-table-tennis"
-          >Play Game</v-btn
-        >
-        <v-btn
-          v-else
+          :disabled="store.selectedUserProfile?.status !== 'ONLINE'"
           class="channelSettings-content-buttons-btn"
           append-icon="mdi-table-tennis"
           >Play Game</v-btn
