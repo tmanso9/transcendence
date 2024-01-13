b18347ffe13b45ab89f3b712106df51eb1432902
tmanso9 <77019593+tmanso9@users.noreply.github.com>
2024-01-13 14:30:37 +0000

    Fix gamestats after merge (#67)

--- frontend/src/components/accountPage/AccountWrapper.vue
+++ frontend/src/components/accountPage/AccountWrapper.vue
@@ -20,13 +20,12 @@
     :class="mdAndUp ? 'account__panels__md' : ''"
   >
     <friends-wrapper
-      class=""
       :account="account"
       @friend-request="handleFriendRequest"
       :isSelf="isSelf"
     />
-    <stats-wrapper class="" :stats="computedStats" :rank="account.rank" />
-    <match-history class="" />
+    <stats-wrapper :account="account" />
+    <match-history />
   </v-expansion-panels>
 </template>
 
@@ -51,15 +50,6 @@ const router = useRouter();
 
 const cookies = inject<VueCookies>("$cookies");
 const props = defineProps(["account", "myFriends", "connections"]);
-
-const computedStats = computed(() => {
-  const { wins, losses, points } = props.account;
-  return {
-    wins: { name: "wins", value: wins },
-    losses: { name: "losses", value: losses },
-    points: { name: "points", value: points },
-  };
-});
 
 const isSelf = computed(() => {
   return user.username && user.username === props.account.username;
