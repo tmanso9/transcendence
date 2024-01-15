7840133b57e38f31f6766fcf3dcdb2845670a906
polltter <miguelr.silva@outlook.pt>
2024-01-15 13:44:15 +0000

    Writing Game data to database (#72)

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import {GameController} from "./game.controller";
 import {GameService} from "./game.service";
 import {gameGateway} from "./game.gateway";
+import {JwtService} from "@nestjs/jwt";
 
 @Module({
     imports: [],
     controllers: [GameController],
-    providers: [GameService, gameGateway],
+    providers: [GameService, gameGateway, JwtService],
     exports: [GameService]
 })
 export class GameModule {}
--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import {clearInterval} from "timers";
+import {GameBackendService} from "../gameBackend/gameBackend.service";
+import {PrismaService} from "../prisma/prisma.service";
+import {ConfigService} from "@nestjs/config";
+import {Player} from "./game.gateway";
 
 class baseObject {
   constructor(public x: number, public y: number, public color: string, public collidable: boolean = true, public orientation: string = 'horizontal') {}
@@ -76,9 +80,13 @@ class Ball extends baseObject {
     }
   }
 }
+
+
+const scoreToWin = 1;
 @Injectable()
 export class GameService {
 
+    static gameBackend: GameBackendService = new GameBackendService(new PrismaService(new ConfigService<Record<string, unknown>, false>()));
     private ball;
     private paddle1;
     private paddle2;
@@ -90,7 +98,7 @@ export class GameService {
     public map = new Map<string, paddle>();
     private intervalId = null;
     public spectators: string[] = [];
-    public score = new Map<string, number>();
+    public score = new Map<string, Player>();
     private lineWidth;
     private paddleWidth;
     private paddleHeight;
@@ -116,20 +124,20 @@ export class GameService {
       this.paddle2 = new paddle(width - 2 * this.paddleWidth, this.paddleStartY, this.paddleWidth, this.paddleHeight, 'red', 'vertical');
       this.ball = new Ball(width / 2, height / 2, width / 200 * 3, width / 200, width / 200, 'white');
       this.elements = [this.hTop, this.hBottom, this.vLeft, this.vRight, this.paddle1, this.paddle2, this.ball];
-      this.score.set('paddle1', 0);
-      this.score.set('paddle2', 0);
+      this.score.set('paddle1', {username: '',id: '', score: 0});
+      this.score.set('paddle2', {username: '',id: '', score: 0});
     }
     checkCollision() {
       return (this.ball as Ball).checkCollision(this.elements, this.width, this.height, this.width / 200, this.width / 200);
     }
 
-    registerPlayer(id: string) {
+    registerPlayer(id: string, player: Player) {
       if (this.map.size == 0) {
-        console.log('player1: ', id);
         this.map.set(id, this.paddle1);
-        console.log(this.map)
+        this.score.set('paddle1', player);
       } else if (this.map.size == 1){
         this.map.set(id, this.paddle2);
+        this.score.set('paddle2', player);
       } else {
         this.spectators.push(id);
         return false;
@@ -144,7 +152,6 @@ export class GameService {
 
     movePaddle(id: string, payload: string) {
       if (payload == 'w') {
-        console.log(id, this.map);
         this.map.get(id).moveUp(this.lineWidth);
       } else if (payload == 's'){
         this.map.get(id).moveDown(this.lineWidth, this.height);
@@ -164,9 +171,13 @@ export class GameService {
       const collidedObject = this.checkCollision();
       if (collidedObject instanceof rectangle && collidedObject.orientation == 'vertical') {
         if (collidedObject.x == 0) {
-          this.score.set('paddle2', this.score.get('paddle2') + 1);
+          const player = this.score.get('paddle2');
+          player.score = player.score + 1;
+          this.score.set('paddle2', player);
         } else {
-          this.score.set('paddle1', this.score.get('paddle1') + 1);
+          const player = this.score.get('paddle1');
+          player.score = player.score + 1;
+          this.score.set('paddle1', player);
         }
       }
       this.ball.x += this.ball.dx;
@@ -183,8 +194,8 @@ export class GameService {
         paddle1: this.getPaddlePosition(1),
         paddle2: this.getPaddlePosition(2),
         score: {
-          paddle1: this.score.get('paddle1'),
-          paddle2: this.score.get('paddle2')
+          paddle1: this.score?.get('paddle1').score || 0,
+          paddle2: this.score?.get('paddle2').score || 0
         }
       };
     }
@@ -192,7 +203,12 @@ export class GameService {
     playG(server, room) {
       if (this.intervalId == null) {
         this.intervalId = setInterval( () => {
-          server.to(room).emit('gameState', this.getGameState() as any);
+          if (this.score.get('paddle1').score == scoreToWin || this.score.get('paddle2').score == scoreToWin) {
+            this.finishG(server, room);
+          }
+          else {
+            server.to(room).emit('gameState', this.getGameState() as any);
+          }
           this.moveBall();
         }, 1000 / 60);
       }
@@ -205,6 +221,22 @@ export class GameService {
     }
   }
 
+  finishG(server, room) {
+    const winner = this.score.get('paddle1').score == scoreToWin ? this.score.get('paddle1') : this.score.get('paddle2');
+    const loser = this.score.get('paddle1').score == scoreToWin ? this.score.get('paddle2') : this.score.get('paddle1');
+    console.log("winner: ",winner.id);
+    console.log("loser: ",loser.id);
+    console.log("loserScore: ",loser.score);
+    GameService.gameBackend.createGame({
+      winnerId: "85953ea5-09e6-4ff8-8434-9b785537213a",
+      loserId: "85953ea5-09e6-4ff8-8434-9b785537213a",
+      loserScore: loser.score
+    }).then(r => (console.log(r)));
+    server.to(room).emit('gameOver', winner.username);
+    this.reset();
+    server.to(room).emit('gameState', this.getGameState() as any);
+    this.pauseG();
+  }
 
     reset() {
       this.ball.x = this.width / 2;
