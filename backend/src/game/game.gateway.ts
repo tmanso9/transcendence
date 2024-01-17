f7893de178e28812e29f8a4842a70ae2dd36ae06
polltter <miguelr.silva@outlook.pt>
2024-01-17 16:15:37 +0000

    Prev containers (#95)

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -10,6 +10,7 @@ import {GameService} from "./game.service";
 import {JwtService} from "@nestjs/jwt";
 import {PrismaService} from "../prisma/prisma.service";
 import {paddle} from "./game.service";
+import {addWaitHandler} from "pactum/src/exports/handler";
 
 
 export interface Player {
@@ -68,22 +69,28 @@ export class gameGateway implements OnGatewayDisconnect, OnGatewayConnection {
   }
 
   @SubscribeMessage('createRoom')
-  createRoom(client: any, payload: any) {
+  async createRoom(client: any, payload: any) {
     client.join(payload.room);
     this.games.set(payload.room, new GameService());
     this.games.get(payload.room).initGame(payload.width, payload.height);
     this.games.get(payload.room).registerPlayer(client.id, this.connectedUsers.get(client.id));
     this.rooms.set(client.id, payload.room);
     this.server.to(payload.room).emit('playerList', Array.from(this.games.get(payload.room).gameState.values()).map(p => p.username) as any);
+    await this.prismService.user.update({where: {id: this.connectedUsers.get(client.id).id}, data: {status: "IN_GAME"}});
     this.server.emit('availableRooms', Array.from(this.games.keys()).map(k => k).filter(k => k.includes("room")) as any);
   }
 
   @SubscribeMessage('joinRoom')
-  joinRoom(client: Socket, payload: string) {
+  async joinRoom(client: Socket, payload: string) {
     if (this.games.has(payload)) {
       client.join(payload);
       if (!this.games.get(payload).registerPlayer(client.id, this.connectedUsers.get(client.id))) {
         client.emit('spectator', true);
+      } else {
+        await this.prismService.user.update({
+          where: {id: this.connectedUsers.get(client.id).id},
+          data: {status: "IN_GAME"}
+        });
       }
       this.server.to(payload).emit('playerList', Array.from(this.games.get(payload).gameState.values()).map(p => p.username) as any);
       this.rooms.set(client.id, payload);
@@ -97,13 +104,15 @@ export class gameGateway implements OnGatewayDisconnect, OnGatewayConnection {
   async addPlayer(client: Socket, payload: string) {
     const decoded = await this.jwtService.decode(payload);
     const prismaUser = await this.prismService.user.findUnique({where: {id: decoded.sub}});
-    const user = {username: prismaUser.username, id: decoded.sub, score: 0, paddle: null as any};
-    this.connectedUsers.set(client.id, user);
+    if (prismaUser) {
+      const user = {username: prismaUser.username, id: decoded.sub, score: 0, paddle: null as any};
+      this.connectedUsers.set(client.id, user);
+    }
     client.emit('availableRooms', Array.from(this.games.keys()).map(k => k).filter(k => k.includes("room")) as any);
   }
 
 
-  handleDisconnect(client: any): any {
+  async handleDisconnect(client: any) {
     const room = this.rooms.get(client.id);
     if (room) {
       const game = this.games.get(room);
@@ -111,25 +120,29 @@ export class gameGateway implements OnGatewayDisconnect, OnGatewayConnection {
         this.pauseGame(client, null);
         //fazer com que o outro jogador ganhe 10-0
         if (game.gameState.size == 2) {
-          if (!game.finished){
+          if (!game.finished) {
             Array.from(game.gameState.values()).forEach(p => {
               p == game.gameState.get(client.id) ? p.score = 0 : p.score = 10
             });
           }
           //avisar a room que o outro jogador ganhou por desistencia/disconnect
-          this.server.to(room).emit('gaveUpOrDisconnected', {quitter: game.gameState.get(client.id).username, winner: Array.from(game.gameState.values()).find(p => p != game.gameState.get(client.id)).username});
-          if (!game.finished){
+          this.server.to(room).emit('gaveUpOrDisconnected', {
+            quitter: game.gameState.get(client.id).username,
+            winner: Array.from(game.gameState.values()).find(p => p != game.gameState.get(client.id)).username
+          });
+          if (!game.finished) {
             game.finishG(this.server, room);
           }
           Array.from(game.gameState.values()).forEach(p => {
             p.score = 0;
           });
-          console.log(game.getGameState());
+
           this.server.to(room).emit('gameState', game.getGameState() as any);
         }
         //emitir o kick para os players
         setTimeout(() => {
-        this.server.to(room).emit('kick', null);}, 1500);
+          this.server.to(room).emit('kick', null);
+        }, 1500);
         //remover o jogo da lista de jogos
         //fechar o room
       } else if (game && game.spectators.includes(client.id)) {
@@ -138,6 +151,10 @@ export class gameGateway implements OnGatewayDisconnect, OnGatewayConnection {
       this.rooms.delete(client.id);
       this.games.delete(room);
     }
+    await this.prismService.user.update({
+      where: {id: this.connectedUsers.get(client.id).id},
+      data: {status: "ONLINE"}
+    });
     this.connectedUsers.delete(client.id);
     this.server.emit('availableRooms', Array.from(this.games.keys()).map(k => k).filter(k => k.includes("room")) as any);
   }
